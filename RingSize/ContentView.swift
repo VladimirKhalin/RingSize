77a0fe25252f0f1f1d83b887f4bf700f72b6a487
VK <art.nebar@mail.ru>
2024-02-09 15:53:46 +0300

    Initial Commit

---
+++ RingSize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RingSize
+//
+//  Created by Vladimir Khalin on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
